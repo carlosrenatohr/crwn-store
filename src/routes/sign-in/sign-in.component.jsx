8d8241c24333a16f1dd6408127288d2cab5d9d51
Carlos Hernandez <honchkrow1995@gmail.com>
2022-08-10 15:09:30 -0600

    fix(logic): Add Google Redirect sign in method (ES6 method from comments)

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,17 +1,30 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
+import { useEffect } from 'react';
+import { getRedirectResult } from 'firebase/auth';
+import { auth, signInWithGooglePopup, signInWithGoogleRedirect, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
 
 const SignIn = () => {
     
+    useEffect( () => async () => { 
+        // (async () => {
+            const response = await getRedirectResult(auth);
+            console.log(response);
+            if (response) {
+                const userDocRef = await createUserDocumentFromAuth(response.user);                
+            }
+            
+        // }) ()
+    }, []);
+
     const logGoogleUser = async () => {
         const response = await signInWithGooglePopup();
         const userDocRef = await createUserDocumentFromAuth(response.user);
-        // console.log(response);
     }
 
     return(
         <div>
             <h1>Sign in</h1>
             <button onClick={logGoogleUser}>Sign in With Google</button>
+            <button onClick={signInWithGoogleRedirect}>Sign in With Redirect</button>
         </div>
     );
 }
