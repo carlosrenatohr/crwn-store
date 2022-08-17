a0e5a5de5b889388f232449bbb1e4ad099b7ced1
Carlos Hernandez <honchkrow1995@gmail.com>
2022-08-16 19:11:46 -0600

    feat(cart): Complete cart components logic + addToCart feature

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,11 +1,18 @@
+import { useContext } from "react";
 import ButtonControl from "../button-control/button-control.component";
+
+import CartItem from '../card-item/cart-item.component';
+import { CartContext } from '../../contexts/cart.context';
 import './cart-dropdown.styles.scss';
 
 const CartDropdown = () => {
-    
+    const { cartItems } = useContext(CartContext);
+
     return (
         <div className="cart-dropdown-container">
-            <div className="cart-items" />
+            <div className="cart-items">
+                { cartItems.map(item => <CartItem key={item.id} cartItem={item}/>) }
+            </div>
             <ButtonControl>Go to Checkout</ButtonControl>
         </div>
     )
--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,8 +1,13 @@
+import { useContext } from 'react';
+import { CartContext } from '../../contexts/cart.context';
+
 import ButtonControl from '../button-control/button-control.component';
 import './product-card.styles.scss';
 const ProductCard = ({product}) => {
-
     const {name, price, imageUrl} = product;
+    const { addItemToCart } = useContext(CartContext);
+    const addProductToCart = () => addItemToCart(product);
+    
     return (
         <div className="product-card-container">
             <img src={imageUrl} alt={`${name}`} />
@@ -10,7 +15,7 @@ const ProductCard = ({product}) => {
                 <div className="name">{name}</div>
                 <div className="price">{price}</div>
             </div>
-            <ButtonControl buttonType="inverted">Add to Cart</ButtonControl>
+            <ButtonControl buttonType="inverted" onClick={addProductToCart} >Add to Cart</ButtonControl>
         </div>
     );
 }
